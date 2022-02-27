19a6f5d7439b792110159d6e5c758c7b26bba81e
SergeyEvtush <evtushru208@gmail.com>
2022-02-27 15:27:51 +0200

    accordeon  js

--- js/app.js
+++ js/app.js
@@ -116,3 +116,18 @@ e.preventDefault();//отключаем работу ссылки
 			}
 		}
 }
+/*аккордеон */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
