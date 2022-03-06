8ac2b5e3dbaf8ee4fce4c780590f418b991f1cd3
SergeyEvtush <evtushru208@gmail.com>
2022-03-06 12:32:24 +0200

    тщ

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ if(menuArrows.length>0){
 for(let index=0;index<menuArrows.length;index++){
 const menuArrow=menuArrows[index];
 menuArrow.addEventListener("click",function(e){
-menuArrow.parentElement.classList.toggle('_active');
+menuArrow.parentElement.classList.toggle('menu_active');
 });
 }
 
